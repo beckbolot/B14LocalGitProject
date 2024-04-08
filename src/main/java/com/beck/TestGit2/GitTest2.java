f73250fe4779eaed43e8be6e3f4549720ce4cf29
Bekbolot E <bekoxna@gmail.com>
2024-04-08 11:46:31 +0600

    GitTest2 updated

--- src/main/java/com/beck/TestGit2/GitTest2.java
+++ src/main/java/com/beck/TestGit2/GitTest2.java
@@ -3,6 +3,7 @@ package com.beck.TestGit2;
 public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("GitTest2");
+        System.out.println("GitTest2-2");
     }
 
 }
