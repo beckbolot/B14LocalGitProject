c02496793b4002440fe8a7d28079706a6cca06fc
Bekbolot E <bekoxna@gmail.com>
2024-04-08 12:37:35 +0600

    Bekbolot file updated, GitTest1 updated

--- src/main/java/com/beck/TestGit/GitTest1.java
+++ src/main/java/com/beck/TestGit/GitTest1.java
@@ -6,5 +6,6 @@ public class GitTest1 {
         System.out.println("Test2");
         System.out.println("Test3");
         System.out.println("Test4");
+        System.out.println("Test5 file after creating branch");
     }
 }
