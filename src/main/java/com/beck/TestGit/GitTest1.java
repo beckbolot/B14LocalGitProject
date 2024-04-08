5546bc48d5eba289d42711850fecc9043a49be04
Bekbolot E <bekoxna@gmail.com>
2024-04-08 13:05:13 +0600

    GitTest1.java updated in bekbolot branch

--- src/main/java/com/beck/TestGit/GitTest1.java
+++ src/main/java/com/beck/TestGit/GitTest1.java
@@ -2,7 +2,7 @@ package com.beck.TestGit;
 
 public class GitTest1 {
     public static void main(String[] args) {
-        System.out.println("Test1");
+        System.out.println("Test1 updated in bekbolot branch");
         System.out.println("Test2");
         System.out.println("Test3");
         System.out.println("Test4");
