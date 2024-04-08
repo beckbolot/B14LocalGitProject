070064590f02428ac91c50f23e91e3d7043e30da
Bekbolot E <bekoxna@gmail.com>
2024-04-08 11:10:46 +0600

    All 4 file are uploaded

---
+++ src/main/java/com/beck/TestGit/GitTest1.java
@@ -0,0 +1,9 @@
+package com.beck.TestGit;
+
+public class GitTest1 {
+    public static void main(String[] args) {
+        System.out.println("Test1");
+        System.out.println("Test2");
+        System.out.println("Test2");
+    }
+}
