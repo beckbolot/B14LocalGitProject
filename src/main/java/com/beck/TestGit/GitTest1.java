816183ae16c9cb1ee6f59fbd2f5c1ddc97f8b089
Bekbolot E <bekoxna@gmail.com>
2024-04-08 14:30:12 +0600

    GitTest1.java updated and Login func uploaded

--- src/main/java/com/beck/TestGit/GitTest1.java
+++ src/main/java/com/beck/TestGit/GitTest1.java
@@ -7,5 +7,6 @@ public class GitTest1 {
         System.out.println("Test3");
         System.out.println("Test4");
         System.out.println("Test5 file after creating branch");
+        System.out.println("in bekbolot2 branch");
     }
 }
